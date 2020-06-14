e73b94ae8becd428b5b7f359a41c0bc1ba5ac1f8
0TH0N <jbator777@gmail.com>
2020-06-14 12:57:44 +0300

    Знакомство с pagination и прочими механизмами.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,11 @@
 </head>
 <body>
 <div class="container mt-4">
+    <div>
+        <span>
+            <a href="{{ route('articles.index') }}">Articles</a>
+        </span>
+    </div>
     <h1>@yield('header')</h1>
     <div>
         @yield('content')
